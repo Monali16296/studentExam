782bf720d2ef0759703c2e24be29eb78cf208293
Monali Hingu <monali16296@gmail.com>
2019-06-22 18:03:22 +0530

    combine, concat, contains, diff, crossjoin, diffassoc

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,15 +7,16 @@ use App\Company;
 use Illuminate\Support\Facades\DB;
 
 /*
- *global scope 
+ * global scope 
  */
+
 //use App\Scopes\AgeScope;
-class CompanyController extends Controller
-{
+class CompanyController extends Controller {
+
     //
     public function getCompanyList() {
         $company = Company::with('manager')->get();
-        
+
         /*
          * remove models which have id 1 and retrieve company names (collection)
          */
@@ -29,22 +30,22 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_r($names);
 //        exit;
-        
+
         /*
          * if you want to print sql
-         */        
+         */
 //        DB::enableQueryLog();
 //        $company = Company::with('manager')->get();
 //        $query = DB::getQueryLog();
 //        print_r($query);
-        
+
         /*
          * retieving data from database just like get()
          */
 //        $company = Company::all();
 //        echo '<pre>';
 //        print_R($company);exit;
-        
+
         /*
          * for retrieving only desired no of records
          */
@@ -52,7 +53,7 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_R($company);
 //        exit;
-        
+
         /*
          * if want to process thousands of record
          */
@@ -62,7 +63,7 @@ class CompanyController extends Controller
 //            }
 //        });
 //        exit;
-        
+
         /*
          * cursor only executes a single query which is used when processing large amounts of data
          */
@@ -70,7 +71,7 @@ class CompanyController extends Controller
 //            echo $flight->name;
 //        }
 //        exit;
-        
+
         /*
          * finding records with primary key
          */
@@ -78,26 +79,26 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_R($a);
 //        exit;
-        
+
         /*
          * if want to throw an exception if model not found
          */
 //        $a = Company::findOrFail(3);
 //        print_r($a);
 //        exit;
-        
+
         /*
          * counting records
          */
 //        print_r(Company::where('id', 1)->count());
 //        exit;
-        
+
         /*
          * max price
          */
 //        print_r(Company::sum('id'));
 //        exit;
-        
+
         /*
          * inserting records. no need to setup timestamps manually
          */
@@ -105,7 +106,7 @@ class CompanyController extends Controller
 //        $a->name = 'aa';
 //        $a->save();
 //        exit;
-        
+
         /*
          * updating record
          */
@@ -113,8 +114,8 @@ class CompanyController extends Controller
 //        $a->name = 'cd';
 //        $a->save();
 //        exit;
-        
-         /*
+
+        /*
          * inserting new record in db and retriving it . 
          * make sure timestamps columns should be there for using create
          */
@@ -129,46 +130,46 @@ class CompanyController extends Controller
 //        $a->fill(['name' => 'abcd']);
 //        $a->save();
 //        exit;
-        
+
         /*
          * if record found with same name in db then won't be inserted otherwise new record inserted
          */
 //        $a = Company::firstOrCreate(['name' => 'abc']);
 //        exit;
-        
+
         /*
          * same as firstorcreate but have to call save() for save record
          */
 //        $a = Company::firstOrNew(['name' => 'abc']);
 //        $a->save();
 //        exit;        
-        
+
         /*
          * if want to update existing record or to add new record if not found
          * here name will be updated to aa
          */
 //        $a = Company::updateOrCreate(['name' => 'TCS', 'id' => 2], ['name' => 'aa']);
 //        exit;
-        
+
         /*
          * for deleting record
          */
 //        $a = Company::find(15);
 //        $a->delete();
 //        exit;
-        
+
         /*
          * want to destroy by primary key without retrieving
          */
 //        Company::destroy(16);
 //        exit;        
-        
+
         /*
          * mass delete
          */
 //        Company::where('id',3)->delete();
 //        exit;
-        
+
         /*
          * deleted columns will also be retrieved.
          */
@@ -176,7 +177,7 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_r($a);
 //        exit;
-        
+
         /*
          * retrieve only deleted records
          */
@@ -184,28 +185,28 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_r($a);
 //        exit;
-        
+
         /*
          * want to restore(set deleted_at to null) soft deleted record
          */
 //        Company::where('id', 3)->restore();
 //        exit;
-        
-        
+
+
         /*
          * use of agescope to retrieve id > 1
          */
 //        echo '<pre>';
 //        print_r(Company::all());
 //        exit;
-        
+
         /*
          * remove global scope for a give query
          */
 //        echo '<pre>';
 //        print_r(Company::withoutGlobalScope(AgeScope::class)->get());
 //        exit;
-        
+
         /*
          * remove global scope
          * in this age method is defined in company model
@@ -213,39 +214,38 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_r(Company::withoutGlobalScope('age')->get());
 //        exit;
-        
+
         /*
          * removing all global scopes
          */
 //        echo '<pre>';
 //        print_r(Company::WithoutGlobalScopes()->get());
 //        exit;
-        
+
         /*
          * using local scopes
          */
 //        echo '<pre>';
 //        print_r(Company::popular()->get());
 //        exit;
-        
+
         /*
          * using dynamic scopes
          */
 //        echo '<pre>';
 //        print_r(Company::ofType(2)->get());
 //        exit;
-        
+
         /*
          * avg on collection
          */
 //        $a = collect([['id' => 1], ['id' => 3]])->avg('id');
 //        print_r($a);
 //        exit;
-        
 //        $a = collect([1, 3, 5, 6])->avg();
 //        print_R($a);
 //        exit;
-        
+
         /*
          * chunk method useful for grid view
          */
@@ -257,7 +257,7 @@ class CompanyController extends Controller
 //               echo $c."</br>";
 //           }
 //        }exit;
-    
+
         /*
          * The collapse method collapses a collection of arrays into a single, flat collection
          */
@@ -266,7 +266,86 @@ class CompanyController extends Controller
 //        echo '<pre>';
 //        print_R($collapsed->all());
 //        exit;
-        
+
+        /*
+         * The combine method combines the values of the collection, as keys, with the values of another array or collection
+         */
+//        $collection = collect(['name', 'age']);
+//        $combined = $collection->combine(['George', 39]);
+//        echo '<pre>';
+//        print_r($combined->all());
+//        exit;
+
+        /*
+         * The concat method appends the given array or collection values onto the end of the collection
+         */
+//        $collection = collect(['john doe']);
+//        $concatenated = $collection->concat(['jogn doe'])->concat(['name' => 'johnny doe']);
+//        echo '<pre>';
+//        print_r($concatenated->all());
+//        exit;
+
+        /*
+         * The contains method determines whether the collection contains a given item
+         */
+//        $collection = collect(['name' => 'desk', 'age' => 100]);
+//        print_R($collection->contains('desk')); //true
+//        print_R($collection->contains('abc')); //false
+//        exit;
+
+        /*
+         * The count method returns the total number of items in the collection
+         */
+//        $collection = collect([1, 2, 3, 4, 5]);     
+//        print_r($collection->count());
+//        exit;
+
+        /*
+         * cartesian product
+         */
+//        $collection = collect([1,2]);
+//        $matrix = $collection->crossJoin(['a', 'b']);
+//        echo '<pre>';
+//        print_r($matrix->all());
+//        exit;        
+
+        /*
+         * The dd method dumps the collection's items and ends execution of the script
+         */
+//        $collection = collect(['john doe', 'monu']);
+//        echo '<pre>';
+//        print_r($collection->dd());
+//        exit;
+
+        /*
+         *  This method will return the values in the original collection that are not present in the given collection
+         */
+//        $collection = collect([1, 2, 3, 4, 5]);
+//        $diff = $collection->diff([2, 4, 6, 8]);
+//        echo '<pre>';
+//        print_R($diff->all());
+//        exit;
+
+        /*
+         * This method will return the key / value pairs in the original collection that are not present in the given collection
+         */
+//        $collection = collect([
+//            'color' => 'orange',
+//            'remain' => 6,
+//            'type' => 'fruit'
+//        ]);
+//        $diff = $collection->diffAssoc([
+//            'color' => 'yellow',
+//            'type' => 'fruit',
+//            'remain' => 3,
+//            'used' => 6
+//        ]);
+//        echo '<pre>';
+//        print_R($diff->all());
+//        exit;
+
+
         return view('forms.companies', compact('company'));
     }
+
 }
