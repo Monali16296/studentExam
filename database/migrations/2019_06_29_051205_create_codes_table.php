6ad789bb7c77304f129327937e314a725db4efbe
Monali Hingu <monali16296@gmail.com>
2019-06-29 11:51:24 +0530

    upgraded laravel version from 5.7 to 5.8, foreign key concept , foreign key should contain same type as primary key otherwise migration wouldn't run - one to one relationship.

---
+++ database/migrations/2019_06_29_051205_create_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('code');
+            $table->bigInteger('employee_id')->unsigned();
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
